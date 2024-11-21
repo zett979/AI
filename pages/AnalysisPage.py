5ce3ba7675598a8d3e3eeaa930c6552d380a6bcc
nyinyi979 <nyinyi095062687@gmail.com>
2024-11-21 21:50:33 +0700

    INIT

---
+++ pages/AnalysisPage.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from components.Base import BaseStyle, Theme, ButtonStyle
+from streamlit_elements import elements, mui, html
+
+st.set_page_config(page_title="Analysis")
+
+BaseStyle()
+
+
+def main():
+    st.write("Analysis Page")
+    st.button("SAMPLE")
+    with elements("multiple_children"):
+        mui.Button(
+            "Button",
+            sx=ButtonStyle(ghost=False, size="sm", variant="primary"),
+            elevation=0,
+            disableRipple=True,
+            className="sample",
+        )
+
+
+main()
